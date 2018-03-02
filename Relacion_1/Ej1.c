2972965375b4a72bcfb259a6ff758cacf77e57cc
jesuspa98 <jesusparejo98@gmail.com>
2018-03-02 13:31:46 +0100

    Primera relacion de ejercicios (TEMA 2)

---
+++ Relacion_1/Ej1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void pointer(int *ptr){
+	printf("Lo que contiene la variable es: %d\n", *ptr);
+}
+
+int main(){
+	int x = 10;
+	int *ptr = &x;
+	printf("Ejercicio 1\n");
+	pointer(ptr);
+
+	return 0;
+}
