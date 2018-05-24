1795688610f8301ce16718c405d473a5f4a702b0
Alkesst <alec.augal@gmail.com>
2018-05-24 22:16:28 +0200

    Added a little example of synchronized exercises

--- Alec/Concurrencia/Practica4/src/exercise0/GardenSync.java
+++ Alec/Concurrencia/Practica4/src/exercise0/GardenSync.java
@@ -1,4 +1,48 @@
 package exercise0;
 
 public class GardenSync {
+    public static class Counter {
+        private volatile int cont = 0;
+
+        public synchronized void increase(int c){
+            cont += c;
+        }
+
+        public int cont(){
+            return cont;
+        }
+    }
+
+    public static class Door extends Thread{
+        private Counter counter;
+
+        public Door(Counter counter){
+            this.counter = counter;
+        }
+
+        public void run(){
+            for (int i = 0; i < 100; i++){
+                counter.increase(1);
+                System.out.println("Value increased... " + counter.cont());
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Counter count = new Counter();
+        Door[] doors = new Door[25];
+        for (int i = 0; i < 25; i++){
+            doors[i] = new Door(count);
+        }
+        for (int i = 0; i < 25; i++){
+            doors[i].start();
+        }
+        try{
+            for (int i = 0; i < 25; i++){
+                doors[i].join();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
